aaee4b8060497d89613ae24da99c180d6fb8fb89
Ethan Biggs <ethanscott402@gmail.com>
2025-02-03 11:00:54 -0700

    Add project files.

---
+++ spellstoringitem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace spellstoringitem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
