c17725a32d78bb7906cc62f1aed530c3b9d66276
Ethan Biggs <ethanscott402@gmail.com>
2025-02-05 19:10:23 -0700

    Full assignment part 2 and animation

--- spellstoringitem/wwwroot/js/site.js
+++ spellstoringitem/wwwroot/js/site.js
@@ -1,4 +1,41 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// Calculator logic
+$('#calculate-btn').on('click', function () {
+    var hours = parseFloat($('#hours').val());
+    if (isNaN(hours) || hours <= 0) {
+        $('#hours-error').text('Please enter a positive number');//update the error message
+        return;
+    } //check if hours is a number and greater than 0
+    var rate = parseFloat($('#rate').val());
+    var total = hours * rate;
+    $('#total').val(`$${total.toFixed(2)}`);//update the total
+    $('#total').css('color', '#000');//update the total color
+    $('#hours-error').text('');//clear the error message if any
+    return false;
+});
 
-// Write your JavaScript code.
+// Animation
+$(document).ready(function () {
+    // Hide the images
+    $("#char1, #char2, #char3").css("opacity", 0);
+
+    // Fade in the images one at a time
+    var images = $("#char1, #char2, #char3");
+    var delay = 1000;
+    images.each(function () {
+        $(this).delay(delay).fadeTo(2000, 1);
+        delay += 500; // increase the delay by 500ms for each image
+    });
+});
+
+$(document).ready(function () {
+    // Hide the images
+    $("#char1-name, #char2-name, #char3-name").css("opacity", 0);
+
+    // Fade in the images one at a time
+    var images = $("#char1-name, #char2-name, #char3-name");
+    var delay = 1000;
+    images.each(function () {
+        $(this).delay(delay).fadeTo(2000, 1);
+        delay += 500; // increase the delay by 500ms for each image
+    });
+});
