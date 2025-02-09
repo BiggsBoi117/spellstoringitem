f98d4d47124aa1635ea0612ecb88f29b8dd0aaef
Ethan Biggs <ethanscott402@gmail.com>
2025-02-08 19:01:37 -0700

    cleanup for mission 6 videos

--- spellstoringitem/Controllers/HomeController.cs
+++ spellstoringitem/Controllers/HomeController.cs
@@ -6,12 +6,13 @@ namespace spellstoringitem.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        //logger will be implemented later to track database interactions
+        //private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
 
         public IActionResult Index()
         {
@@ -23,10 +24,5 @@ namespace spellstoringitem.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
